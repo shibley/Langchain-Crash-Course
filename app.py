0a48155d981fade38595610f67f2c4cfa269d2a4
Shibley <shibley@gmail.com>
2023-06-28 20:21:48 -0700

    apikey spelling

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
 
-os.environ['OPENAI_API_KEY'] = st.secrets.aspikey
+os.environ['OPENAI_API_KEY'] = st.secrets.apikey
 
 # App framework
 st.title('🦜🔗 YouTube GPT Creator')
